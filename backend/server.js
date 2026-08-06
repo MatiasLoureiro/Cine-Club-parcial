ede7deae18ce2b2c258ea448c9fffc42e514a9a4
MatiasLoureiro <matiasloureiro90gmail.com>
2026-08-05 23:55:48 -0300

    Usar variables de entorno en el servidor

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   res.send("CineClub API funcionando");
