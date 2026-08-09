a979e49a8e8f86479bc82ae9764a1c685472ee72
MatiasLoureiro <matiasloureiro90gmail.com>
2026-08-09 15:09:29 -0300

    Agregar detalle de películas con TMDB

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,30 @@ app.get("/api/movies/search", async (req, res) => {
   }
 });
 
+app.get("/api/movies/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${id}`,
+      {
+        params: {
+          api_key: process.env.TMDB_API_KEY,
+          language: "es-ES",
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error al consultar TMDB:", error.message);
+
+    res.status(500).json({
+      error: "No se pudo obtener la película",
+    });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en http://localhost:${PORT}`);
 });
